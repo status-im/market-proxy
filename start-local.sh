71460db20550502e513a65ece8050df3d2e27cf2
markoburcul <marko@status.im>
2025-04-10 16:20:32 +0200

    secrets: remove folder and adapt scripts
    Remove unnecessary start.sh script as well.

--- start-local.sh
+++ start-local.sh
@@ -3,6 +3,25 @@
 # Set CORS origin for local development
 export CORS_ORIGIN="http://localhost:3000"
 
+if [ ! -d "./secrets" ]; then
+    mkdir -p "./secrets"
+fi
+
+if [ ! -f "./secrets/.htpasswd" ]; then
+    # Create .htpasswd with the user 'test' and password 'test'
+    echo "test:$(openssl passwd -apr1 test)" > ./secrets/.htpasswd
+fi
+
+if [ ! -f "./secrets/coingecko_api_tokens.json" ]; then
+    cat <<EOL > ./secrets/coingecko_api_tokens.json
+{
+  "api_tokens": [
+    "Coingecko-token"
+  ]
+}
+EOL
+fi
+
 # Stop and remove existing containers AND volumes
 docker compose -f docker-compose.local.yml down --volumes
 
