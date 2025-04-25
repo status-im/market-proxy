1c04d8cb61a337a759155207a743ce6086aa4536
Andrey Bocharnikov <andrey.bocharnikov@gmail.com>
2025-04-25 17:05:13 +0400

    feat(market-fetcher): separate config per service

--- market-fetcher/config/config.go
+++ market-fetcher/config/config.go
@@ -1,30 +1,14 @@
 package config
 
 import (
-	"encoding/json"
 	"io/ioutil"
-	"os"
 
 	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
-	CoingeckoLeaderboardFetcher struct {
-		UpdateIntervalMs int    `yaml:"update_interval_ms"`
-		TokensFile       string `yaml:"tokens_file"`
-		Limit            int    `yaml:"limit"`
-		RequestDelayMs   int    `yaml:"request_delay_ms"` // Delay between requests in milliseconds
-	} `yaml:"coingecko_leaderboard"`
-
-	TokensFetcher struct {
-		UpdateIntervalMs   int      `yaml:"update_interval_ms"`
-		SupportedPlatforms []string `yaml:"supported_platforms"`
-	} `yaml:"tokens_fetcher"`
-}
-
-type APITokens struct {
-	Tokens     []string `json:"api_tokens"`
-	DemoTokens []string `json:"demo_api_tokens,omitempty"`
+	CoingeckoLeaderboard CoingeckoLeaderboardFetcher `yaml:"coingecko_leaderboard"`
+	TokensFetcher        CoingeckoCoinslistFetcher   `yaml:"coingecko_coinslist"`
 }
 
 func LoadConfig(path string) (*Config, error) {
@@ -36,21 +20,4 @@ func LoadConfig(path string) (*Config, error) {
 	var config Config
 	err = yaml.Unmarshal(data, &config)
 	return &config, err
-}
-
-func LoadAPITokens(filename string) (*APITokens, error) {
-	// Check if file exists
-	if _, err := os.Stat(filename); os.IsNotExist(err) {
-		// File doesn't exist, return empty tokens
-		return &APITokens{Tokens: []string{}}, nil
-	}
-
-	data, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	var tokens APITokens
-	err = json.Unmarshal(data, &tokens)
-	return &tokens, err
 }
