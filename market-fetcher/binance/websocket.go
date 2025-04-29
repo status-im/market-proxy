eda9b362f482264225ec1486b7cbb157be8c935d
Andrey Bocharnikov <andrey.bocharnikov@gmail.com>
2025-04-29 19:55:22 +0400

    feat(market-fetcher): rework websocket

--- market-fetcher/binance/websocket.go
+++ market-fetcher/binance/websocket.go
@@ -1,9 +1,9 @@
 package binance
 
 import (
-	"context"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -22,6 +22,9 @@ type WebSocketClient struct {
 	stopCh    chan struct{}
 	onMessage func(message []byte) error
 	wsURL     string
+	mu        sync.Mutex
+	isRunning bool
+	doneCh    chan struct{}
 }
 
 // NewWebSocketClient creates a new WebSocket client
@@ -33,11 +36,27 @@ func NewWebSocketClient(stopCh chan struct{}, onMessage func(message []byte) err
 		stopCh:    stopCh,
 		onMessage: onMessage,
 		wsURL:     wsURL,
+		isRunning: false,
+		doneCh:    make(chan struct{}),
 	}
 }
 
 // Connect establishes connection to Binance WebSocket API
+// It's safe to call this method multiple times - it will only connect once unless Close() was called
 func (wsc *WebSocketClient) Connect() error {
+	wsc.mu.Lock()
+	defer wsc.mu.Unlock()
+
+	// Don't reconnect if already connected and running
+	if wsc.isConnected() {
+		return nil
+	}
+
+	return wsc.connect()
+}
+
+// connect establishes a new connection (must be called with mutex locked)
+func (wsc *WebSocketClient) connect() error {
 	// Connect to Binance WebSocket data stream
 	conn, _, err := websocket.DefaultDialer.Dial(wsc.wsURL, nil)
 	if err != nil {
@@ -48,11 +67,79 @@ func (wsc *WebSocketClient) Connect() error {
 	conn.SetReadDeadline(time.Now().Add(PONG_TIMEOUT))
 
 	wsc.conn = conn
+
+	// Setup ping/pong handlers
+	wsc.setupPingPong()
+
+	// Reset done channel if needed
+	select {
+	case <-wsc.doneCh:
+		wsc.doneCh = make(chan struct{})
+	default:
+	}
+
+	// Start message loop
+	wsc.isRunning = true
+	go wsc.startMessageLoop()
+
 	return nil
 }
 
-// SetupPingPong sets up ping/pong handlers for the WebSocket connection
-func (wsc *WebSocketClient) SetupPingPong() {
+// closeAndConnect closes the current connection and attempts to reconnect
+func (wsc *WebSocketClient) closeAndConnect() {
+	wsc.mu.Lock()
+	defer wsc.mu.Unlock()
+
+	// Close connection but don't set isRunning to false
+	if wsc.conn != nil {
+		// Signal current loop to stop
+		close(wsc.doneCh)
+
+		// Close connection
+		wsc.conn.Close()
+		wsc.conn = nil
+	}
+
+	// Only attempt to reconnect if we're supposed to be running
+	if !wsc.isRunning {
+		return
+	}
+
+	// Attempt to reconnect
+	if err := wsc.connect(); err != nil {
+		log.Printf("Failed to reconnect to Binance WebSocket: %v", err)
+	}
+}
+
+// Close closes the WebSocket connection
+// It's safe to call this method multiple times
+func (wsc *WebSocketClient) Close() {
+	wsc.mu.Lock()
+	defer wsc.mu.Unlock()
+
+	if !wsc.isRunning {
+		return
+	}
+
+	// Signal loop to stop
+	close(wsc.doneCh)
+
+	// Close connection
+	if wsc.conn != nil {
+		wsc.conn.Close()
+		wsc.conn = nil
+	}
+
+	wsc.isRunning = false
+}
+
+// isConnected returns true if the client is connected
+func (wsc *WebSocketClient) isConnected() bool {
+	return wsc.isRunning && wsc.conn != nil
+}
+
+// setupPingPong sets up ping/pong handlers for the WebSocket connection
+func (wsc *WebSocketClient) setupPingPong() {
 	// Set ping handler to respond with pong containing the same data
 	wsc.conn.SetPingHandler(func(appData string) error {
 		// Reset read deadline
@@ -66,13 +153,13 @@ func (wsc *WebSocketClient) SetupPingPong() {
 	})
 }
 
-// StartMessageLoop begins reading messages from the WebSocket connection
-func (wsc *WebSocketClient) StartMessageLoop(ctx context.Context, reconnectFn func()) {
+// startMessageLoop begins reading messages from the WebSocket connection
+func (wsc *WebSocketClient) startMessageLoop() {
 	for {
 		select {
-		case <-ctx.Done():
-			return
 		case <-wsc.stopCh:
+			return
+		case <-wsc.doneCh:
 			return
 		default:
 			// Read message
@@ -82,7 +169,7 @@ func (wsc *WebSocketClient) StartMessageLoop(ctx context.Context, reconnectFn fu
 					log.Printf("Error reading WebSocket message: %v", err)
 				}
 				// Try to reconnect
-				reconnectFn()
+				wsc.closeAndConnect()
 				continue
 			}
 
@@ -91,13 +178,5 @@ func (wsc *WebSocketClient) StartMessageLoop(ctx context.Context, reconnectFn fu
 				log.Printf("%v", err)
 			}
 		}
-	}
-}
-
-// Close closes the WebSocket connection
-func (wsc *WebSocketClient) Close() {
-	if wsc.conn != nil {
-		wsc.conn.Close()
-		wsc.conn = nil
 	}
 }
